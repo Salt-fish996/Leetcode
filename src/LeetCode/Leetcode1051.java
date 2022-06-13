22385a2c4ba386251d89fcb03c4ef2bce9d61e5d
scu2016hyy <2439735732@qq.com>
2022-06-13 23:01:23 +0800

    feat:20220613

---
+++ src/LeetCode/Leetcode1051.java
@@ -0,0 +1,17 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+public class Leetcode1051 {
+    public int heightChecker(int[] heights) {
+        int[] newHeight = Arrays.copyOf(heights,heights.length);
+        Arrays.sort(newHeight);
+        int res = 0;
+        for (int i = 0; i < heights.length; i++) {
+            if (heights[i] != newHeight[i]) {
+                res++;
+            }
+        }
+        return res;
+    }
+}
--- src/LeetCode/TreeNode.java
+++ src/LeetCode/TreeNode.java
@@ -5,8 +5,8 @@ package LeetCode;
  * @date 2019/8/6 21:08
  */
 public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
     TreeNode(int x) { val = x; }
 }
