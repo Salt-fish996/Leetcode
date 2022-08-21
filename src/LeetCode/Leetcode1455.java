62b084376c33e2e2192469ab475a2497929c004f
huangyuyang.666 <huangyuyang.666@bytedance.com>
2022-08-21 19:10:33 +0800

    feat: 20220821
    Change-Id: Id28b72ee146859b46e39b161455eead9fb18e6ab

---
+++ src/LeetCode/Leetcode1455.java
@@ -0,0 +1,13 @@
+package src.LeetCode;
+
+public class Leetcode1455 {
+    public int isPrefixOfWord(String sentence, String searchWord) {
+        String[] s = sentence.split(" ") ;
+        for (int i = 0; i < s.length; i++) {
+            if (s[i].startsWith(searchWord)) {
+                return i+1;
+            }
+        }
+        return -1;
+    }
+}
